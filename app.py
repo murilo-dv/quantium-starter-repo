be2754e8a40f39f502f84efb146036d2d4fabf24
murilo-dv <150307709+murilo-dv@users.noreply.github.com>
2025-10-30 03:14:09 +1100

    Add bacground colors

--- app.py
+++ app.py
@@ -4,17 +4,25 @@ import pandas as pd
 
 app = Dash()
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 df = pd.DataFrame(pd.read_csv('cleaned_data.csv',))
 
-fig = px.line(df, x='date',y='sales', color='region', markers=True)
+fig = px.line(df, x='date',y='sales', color='region', markers=True, title='Sales by Region Over Time')
 
-app.layout = html.Div(
+
+
+app.layout = html.Div(style={'backgroundColor': colors['background']},
     children=[
-        html.H1(children='Soul Foods Report'),
+        html.Div(children=html.H1(children='Soul Foods Report')),
         html.Div(children='''
         Report of sales, sorted by date and filter by region.
     '''),
         dcc.Graph(
+
             id='Sales',
             figure=fig
         )
